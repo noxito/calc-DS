0156bd52461a8f9a8b230dd9ba4a65890254dfcb
Wille <57924483+noxito@users.noreply.github.com>
2022-03-26 18:41:58 -0300

    Add files via upload

---
+++ include/graph.h
@@ -0,0 +1,8 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <string>
+#include <vector>
+void graph(std::string equation);
+std::string make_equation(std::vector<std::string> equation_split, double x_value);
+#endif
