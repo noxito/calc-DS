e50ef9af9a8000d11e4749ce77229e1197fa5482
Wille <57924483+noxito@users.noreply.github.com>
2022-03-26 18:40:40 -0300

    Delete include directory

--- include/factor.h
+++
@@ -1,6 +0,0 @@
-#ifndef FACTOR_H
-#define FACTOR_H
-
-#include <string>
-std::string factor(std::string equation);
-#endif
--- include/graph.h
+++
@@ -1,8 +0,0 @@
-#ifndef GRAPH_H
-#define GRAPH_H
-
-#include <string>
-#include <vector>
-void graph(std::string equation);
-std::string make_equation(std::vector<std::string> equation_split, double x_value);
-#endif
